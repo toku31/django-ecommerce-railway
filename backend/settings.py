5165f8b3e41810859ea84fad64e11b86ebce8091
garden627 <garden627@yahoo.co.jp>
2023-02-05 17:22:20 +0900

    add

--- backend/settings.py
+++ backend/settings.py
@@ -198,6 +198,8 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'staticfiles/images'
+# MEDIA_ROOT = BASE_DIR / 'static/images'
+
 # STATIC_ROOT = BASE_DIR / 'static'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
